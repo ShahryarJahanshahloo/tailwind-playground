8c6e096c0ae3e18e67fc0467b71e3490dede7237
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-02-21 16:03:37 +0330

    added headlessui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isDark, setDark] = useState(false)
+
+  const toggleDarkHandler = () => {
+    setDark(state => !state)
+  }
+
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <div className={isDark ? 'dark' : ''}>
+        <input
+          type='checkbox'
+          className={`daisy-toggle daisy-toggle-lg ${
+            isDark ? 'shadow-violet-400' : 'bg-white shadow-violet'
+          }`}
+          onClick={toggleDarkHandler}
+          checked={isDark}
+        />
+        <h1 className='dark:bg-black'>Something</h1>
+      </div>
     </div>
   )
 }
